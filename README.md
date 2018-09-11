c368be0ff5ec3ceeca56ab8d0aeedf8365c92d50
KhoirulFajri <muh.khoirulfajri.mkf@gmail.com>
2018-09-11 14:13:48 +0700

    Test best file

---
+++ README.md
@@ -0,0 +1,1 @@
+#Kamset-Web
---
+++ index_admin.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="id" id="home">
+
+<head>
+  <title> Kamasetra UNY </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="css/theme.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="icon" type="”image/png”" href="img/logo.png"> </head>
+
+<body>
+  <!-- NAVBAR -->
+  <nav class="navbar navbar-expand-md bg-primary navbar-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand page-scroll" href="#home">
+        <img src="img/logo.png" height="34px"> Kamasetra UNY </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="page-scroll nav-link text-grey" href="#home"> Home </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="page-scroll nav-link text-grey" href="#divisi"> Divisi </a>
+          </li>
+        </ul>
+        
+        <a class="btn navbar-btn ml-2 text-white btn-dark" href="login.html">
+          <i class="fa d-inline fa-lg fa-user-circle-o"></i> Sign in </a>
+      </div>
+    </div>
+  </nav>
+ 
+  <!-- AKHIR NAVBAR -->
+  <div class="text-center py-4 bg-primary filter-fade-in" style="background-image: url(&quot;img/home.jpg&quot;); background-position: center center; ;background-size:cover;">
+    <div class="container" style="margin-top:40px">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-primary text-md"> Kamasetra UNY </h1>
+          <p style="margin-bottom:40px; color:black;"> Keluarga Mahasiswa Seni Tradisi </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-4 bg-primary filter-dark" style="background-image:url(img/quotes.jpg);">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <center>
+            <h2 class="fa fa-quote-left fa-2x"> KAMASETRA JAYA RAYA, SENTOSA </h2>
+            <p class="fa fa-quote-right fa-2x"> </p>
+          </center>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5" id="divisi">
+    <div class="container">
+      <h3 class="text-center text-primary">Divisi Kamasetra </h3>
+      <hr>
+      <div class="row" style="margin-top:30px;">
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Divisi Penanggung Jawab</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary btn-block">Read more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Divisi Litbang</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary btn-block">Read more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Divisi Kesra</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary btn-block">Read more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row" style="margin-top:30px;">
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Divisi Sarana Prasarana</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary btn-block">Read more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Divisi Karawitan</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary btn-block">Read more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Divisi Tari</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary btn-block">Read more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row" style="margin-top:30px;">
+        <div class="col-md-2"> </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Divisi Theater</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary btn-block">Read more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Divisi embuh</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary btn-block">Read more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-1 bg-dark text-white" id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="mt-3 text-center col-md-12">
+          <a href="https://www.facebook.com" target="_blank">
+            <i class="fa fa-fw fa-facebook fa-lg text-white"></i>
+          </a>
+          <a href="https://twitter.com" target="_blank">
+            <i class="fa fa-fw fa-twitter fa-lg text-white"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank">
+            <i class="fa fa-fw fa-instagram text-white fa-lg"></i>
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 mt-3 text-center">
+          <p>© Copyright 2018 Kamasetra UNY - All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"></script>
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/navbar.js"></script>
+  
+</body>
+
+</html>
